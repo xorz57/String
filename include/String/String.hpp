cc50c599554221de84aa42d4bb26e086741361db
George Fotopoulos <xorz57@gmail.com>
2024-05-18 01:32:36 +0300

    Update

--- include/String/String.hpp
+++ include/String/String.hpp
@@ -1,85 +1,70 @@
 #pragma once
 
+#include <algorithm>
 #include <cstring>
 #include <iostream>
 
-class String
-{
+class String {
 public:
-    String() = default;
+  String() = default;
 
-    ~String()
-    {
-        if (mBuffer)
-        {
-            mLength = 0;
-            delete[] mBuffer;
-            mBuffer = nullptr;
-        }
+  ~String() {
+    if (mBuffer) {
+      mLength = 0;
+      delete[] mBuffer;
+      mBuffer = nullptr;
     }
+  }
 
-    explicit String(const char *str)
-    {
-        mLength = std::strlen(str);
-        mBuffer = new char[mLength + 1];
-        mBuffer[mLength] = '\0';
-        std::strncpy(mBuffer, str, mLength);
-    }
+  explicit String(const char *str) {
+    mLength = std::strlen(str);
+    mBuffer = new char[mLength + 1];
+    mBuffer[mLength] = '\0';
+    std::copy_n(str, mLength, mBuffer);
+  }
 
-    String(const String &other)
-    {
-        mLength = other.mLength;
-        mBuffer = new char[other.mLength + 1];
-        std::strncpy(mBuffer, other.mBuffer, other.mLength);
-        mBuffer[other.mLength] = '\0';
-    }
+  String(const String &other) : mLength(other.mLength) {
+    mBuffer = new char[other.mLength + 1];
+    std::copy_n(other.mBuffer, other.mLength, mBuffer);
+    mBuffer[other.mLength] = '\0';
+  }
 
-    String(String &&other)
-    {
-        mLength = other.mLength;
-        mBuffer = other.mBuffer;
-        other.mLength = 0;
-        other.mBuffer = nullptr;
-    }
+  String(String &&other) noexcept
+      : mLength(other.mLength), mBuffer(other.mBuffer) {
+    other.mLength = 0;
+    other.mBuffer = nullptr;
+  }
 
-    String &operator=(const String &other)
-    {
-        if (this != &other)
-        {
-            mLength = other.mLength;
-            mBuffer = new char[other.mLength + 1];
-            std::strncpy(mBuffer, other.mBuffer, other.mLength);
-            mBuffer[other.mLength] = '\0';
-        }
-        return *this;
+  String &operator=(const String &other) {
+    if (this != &other) {
+      mLength = other.mLength;
+      mBuffer = new char[other.mLength + 1];
+      std::copy_n(other.mBuffer, other.mLength, mBuffer);
+      mBuffer[other.mLength] = '\0';
     }
+    return *this;
+  }
 
-    String &operator=(String &&other)
-    {
-        if (this != &other)
-        {
-            mLength = other.mLength;
-            mBuffer = other.mBuffer;
-            other.mLength = 0;
-            other.mBuffer = nullptr;
-        }
-        return *this;
+  String &operator=(String &&other) noexcept {
+    if (this != &other) {
+      mLength = other.mLength;
+      mBuffer = other.mBuffer;
+      other.mLength = 0;
+      other.mBuffer = nullptr;
     }
+    return *this;
+  }
 
-    size_t length() const
-    {
-        return mLength;
-    }
+  size_t length() const { return mLength; }
 
-    friend std::ostream &operator<<(std::ostream &os, const String &string)
-    {
-        if (string.mBuffer) {
-            os << string.mBuffer;
-        }
-        return os;
+  friend std::ostream &operator<<(std::ostream &os, const String &string) {
+    if (string.mBuffer) {
+      os << string.mBuffer;
     }
+    return os;
+  }
 
 private:
-    char *mBuffer = nullptr;
-    size_t mLength = 0;
+  size_t mLength = 0;
+  char *mBuffer = nullptr;
 };
