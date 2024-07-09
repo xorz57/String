1da5cf0dfbc14dae427f6de47b5c8b7c5b4dce6f
George Fotopoulos <xorz57@gmail.com>
2024-07-09 21:01:18 +0300

    Update

--- include/String/String.hpp
+++ include/String/String.hpp
@@ -9,61 +9,61 @@ public:
     string_t() = default;
 
     ~string_t() {
-        if (mBuffer) {
-            mLength = 0;
-            delete[] mBuffer;
-            mBuffer = nullptr;
+        if (m_buffer) {
+            m_length = 0;
+            delete[] m_buffer;
+            m_buffer = nullptr;
         }
     }
 
     explicit string_t(const char *str) {
-        mLength = std::strlen(str);
-        mBuffer = new char[mLength + 1];
-        mBuffer[mLength] = '\0';
-        std::copy_n(str, mLength, mBuffer);
+        m_length = std::strlen(str);
+        m_buffer = new char[m_length + 1];
+        m_buffer[m_length] = '\0';
+        std::copy_n(str, m_length, m_buffer);
     }
 
-    string_t(const string_t &other) : mLength(other.mLength) {
-        mBuffer = new char[other.mLength + 1];
-        std::copy_n(other.mBuffer, other.mLength, mBuffer);
-        mBuffer[other.mLength] = '\0';
+    string_t(const string_t &other) : m_length(other.m_length) {
+        m_buffer = new char[other.m_length + 1];
+        std::copy_n(other.m_buffer, other.m_length, m_buffer);
+        m_buffer[other.m_length] = '\0';
     }
 
-    string_t(string_t &&other) noexcept : mLength(other.mLength), mBuffer(other.mBuffer) {
-        other.mLength = 0;
-        other.mBuffer = nullptr;
+    string_t(string_t &&other) noexcept : m_length(other.m_length), m_buffer(other.m_buffer) {
+        other.m_length = 0;
+        other.m_buffer = nullptr;
     }
 
     string_t &operator=(const string_t &other) {
         if (this != &other) {
-            mLength = other.mLength;
-            mBuffer = new char[other.mLength + 1];
-            std::copy_n(other.mBuffer, other.mLength, mBuffer);
-            mBuffer[other.mLength] = '\0';
+            m_length = other.m_length;
+            m_buffer = new char[other.m_length + 1];
+            std::copy_n(other.m_buffer, other.m_length, m_buffer);
+            m_buffer[other.m_length] = '\0';
         }
         return *this;
     }
 
     string_t &operator=(string_t &&other) noexcept {
         if (this != &other) {
-            mLength = other.mLength;
-            mBuffer = other.mBuffer;
-            other.mLength = 0;
-            other.mBuffer = nullptr;
+            m_length = other.m_length;
+            m_buffer = other.m_buffer;
+            other.m_length = 0;
+            other.m_buffer = nullptr;
         }
         return *this;
     }
 
-    size_t length() const { return mLength; }
+    size_t length() const { return m_length; }
 
     friend std::ostream &operator<<(std::ostream &os, const string_t &string) {
-        if (string.mBuffer) {
-            os << string.mBuffer;
+        if (string.m_buffer) {
+            os << string.m_buffer;
         }
         return os;
     }
 
 private:
-    size_t mLength = 0;
-    char *mBuffer = nullptr;
+    size_t m_length = 0;
+    char *m_buffer = nullptr;
 };
